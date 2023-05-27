e1fc1af3151a2835ecda54a75b9212efc74318b7
Benzellhub <nsg.vrussell@gmail.com>
2023-05-28 05:31:39 +0800

    InklingV2.5

--- scripts/main.js
+++ scripts/main.js
@@ -147,3 +147,96 @@ function easeInOutCubic(t, b, c, d) {
   t -= 2;
   return (c / 2) * (t * t * t + 2) + b;
 }
+
+updateCartTotal();
+
+document.getElementById('emptycart').addEventListener('click', emptyCart);
+const cartBtns = document.getElementsByClassName('addtocart__btn');
+for (let i = 0; i < cartBtns.length; i++) {
+  cartBtns[i].addEventListener('click', function () {
+    addToCart(this);
+  });
+}
+
+function addToCart(elem) {
+  let sibs = [];
+  let getprice;
+  let getproductName;
+  let cart = [];
+  let stringCart;
+  while ((elem = elem.previousSibling)) {
+    if (elem.nodeType === 3) continue;
+    if (elem.className == 'price') {
+      getprice = elem.innerText;
+    }
+    if (elem.className == 'product__name') {
+      getproductName = elem.innerText;
+    }
+    sibs.push(elem);
+  }
+  const product = {
+    productname: getproductName,
+    price: getprice,
+  };
+  const stringProduct = JSON.stringify(product);
+
+  if (!sessionStorage.getItem('cart')) {
+    cart.push(stringProduct);
+    stringCart = JSON.stringify(cart);
+    sessionStorage.setItem('cart', stringCart);
+    addedToCart(getproductName);
+    updateCartTotal();
+  } else {
+    cart = JSON.parse(sessionStorage.getItem('cart'));
+    cart.push(stringProduct);
+    stringCart = JSON.stringify(cart);
+    sessionStorage.setItem('cart', stringCart);
+    addedToCart(getproductName);
+    updateCartTotal();
+  }
+}
+
+function updateCartTotal() {
+  let total = 0;
+  let price = 0;
+  let items = 0;
+  let productname = '';
+  let carttable = '';
+  if (sessionStorage.getItem('cart')) {
+    var cart = JSON.parse(sessionStorage.getItem('cart'));
+    items = cart.length;
+    for (let i = 0; i < items; i++) {
+      const x = JSON.parse(cart[i]);
+      price = parseFloat(x.price.split('$')[1]);
+      productname = x.productname;
+      carttable += `<tr><td>${productname}</td><td>$${price.toFixed(
+        2
+      )}</td></tr>`;
+      total += price;
+    }
+  }
+  document.getElementById('total').innerHTML = total.toFixed(2);
+  document.getElementById('cart__table').innerHTML = carttable;
+  document.getElementById('items__quantity').innerHTML = items;
+}
+
+function addedToCart(pname) {
+  const message = pname + ' was added to the cart';
+  const notif = document.getElementById('notif');
+  notif.innerHTML = message;
+  if (!notif.classList.contains('message')) {
+    notif.classList.add('message');
+  }
+}
+
+function emptyCart() {
+  if (sessionStorage.getItem('cart')) {
+    sessionStorage.removeItem('cart');
+    updateCartTotal();
+    const notif = document.getElementById('notif');
+    notif.innerHTML = '';
+    if (notif.classList.contains('message')) {
+      notif.classList.remove('message');
+    }
+  }
+}
